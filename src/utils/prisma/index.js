9ca9b99137b291b0f3463d557404fbf4c39e0add
surinnnnnn <didthfls2@naver.com>
2024-09-19 14:08:13 +0900

    Update index.js

--- src/utils/prisma/index.js
+++ src/utils/prisma/index.js
@@ -1,13 +1,13 @@
 import { PrismaClient as gameDataClient } from "../../../prisma/game/generated/gameDataClient/index.js";
 
-export const gameDataClient = new gameDataClient({
+export const gamePrisma = new gameDataClient({
   log: ["query", "info", "warn", "error"],
   errorFormat: "pretty",
 });
 
 import { PrismaClient as userDataClient } from "../../../prisma/user/generated/userDataClient/index.js";
 
-export const userDataClient = new userDataClient({
+export const usersPrisma = new userDataClient({
   log: ["query", "info", "warn", "error"],
   errorFormat: "pretty",
 });
