56e265ed2d85197f3d208e4f10d3c16b0eb6c0b5
Rien3844 <102456478+Rien3844@users.noreply.github.com>
2024-09-19 19:24:30 +0900

    update account.router.js
    닉네임 글자제한, 비밀번호 확인

--- src/routes/account.router.js
+++ src/routes/account.router.js
@@ -1,25 +1,33 @@
 import express from 'express';
-import {gameDataClient} from '@Prisma/client';
+import {usersprisma} from "../utils/Prisma/index.js";
+import authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
 //회원가입 API
-router.post('api/sign-up', async(req,res,next)=>{
+router.post('api/sign-up', authMiddleware, async(req,res,next)=>{
     try{
         const validaion = await usersSchema.validateAsync(req.body);
         const{userid, password, checkpassword, nickname} = validaion;
 
 //닉네임 중복 확인
-        const isExistNickName = await prisma.users.findFirst({
+        const isExistNickName = await usersprisma.users.findFirst({
         where: { nickname},
     });
     if(isExistNickName){
         return res.status(409).json({errorMessage : '이미 존재하는 이름입니다.'});
     }
+    //닉네임 글자 제한
+    if(nickname.length < 3 && nickname.length > 8){
+        return res.status(400).json({errormessage : "닉네임은 3글자 이상, 8글자 이하여야 합니다."});
+    }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
-    const [account, character] = await prisma.$transaction(
+  //비밀번호 확인
+  if(password !== checkpassword){
+    return res.status(400).json({errormessage : "비밀번호와 비밀번호 확인이 일치하지 않습니다."});
+}
+    const [account, character] = await usersprisma.$transaction(
         async (tx) =>{
             const account = await tx.users.create({
                 data:{
@@ -54,13 +62,13 @@ router.post('api/sign-up', async(req,res,next)=>{
 });
 
 //로그인 API
-router.post('/api/sign-in', async (req, res, next) => {
+router.post('/api/sign-in',authMiddleware, async (req, res, next) => {
     try{
         const validaion = await signInSchema.validateAsync(req.body);
         const {userid, password} = validaion;
 
     //유저 존재 유무 확인
-    const account = awiat.prisma.users.findFirst({
+    const account = awiat.usersprisma.users.findFirst({
         where:{userid},
     });
     if(!account){
